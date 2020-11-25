3e1161135505f03d29e62d0a73d0df00b864b1ff
marcoruzzon <marco.ruzzon@iit.it>
2020-11-25 18:58:17 +0100

    default ssh for git clone

--- create_project_CMakeLists.py
+++ create_project_CMakeLists.py
@@ -8,8 +8,8 @@ parser.add_argument('repos_config_state_yaml', help="the yaml file containing re
 parser.add_argument('--cmake_min_version', metavar="VERSION", default="2.8.12", type=str)
 parser.add_argument('--gitlab_advrcloud_base_address', metavar="ADDRESS", help="Address to use for ADVR cloud Gitlab git repositories",
                     default="ssh://git@gitlab.advrcloud.iit.it/", type=str)
-parser.add_argument('--gitlab_advr_base_address', metavar="ADDRESS", help="Address to use for ADVR Gitlab git repositories",
-                    default="ssh://git@gitlab.advr.iit.it/", type=str)
+parser.add_argument('--gitlab_advr_base_address', metavar="ADDRESS", help="Address to use for ADVR Gitlab git repositories", default="ssh://git@gitlab.advr.iit.it/", type=str)
+parser.add_argument('--github_advr_base_address', metavar="ADDRESS", help="Address to use for ADVR Github git repositories", default="ssh://git@github.com/", type=str)
 args = parser.parse_args()
 
 with open(args.repos_config_state_yaml) as file:
@@ -23,6 +23,7 @@ template = "find_or_build_package_with_tag({} {})\n"
 with open('CMakeLists.txt', "w") as f:
 
     f.write("cmake_minimum_required(VERSION {})\n\n".format(args.cmake_min_version))
+    f.write('set(YCM_GIT_GITHUB_BASE_ADDRESS "{}" CACHE STRING "Address to use for ADVR Github git repositories" FORCE)\n'.format(args.github_advr_base_address))
     f.write('set(YCM_GIT_GITLAB_ADVRCLOUD_BASE_ADDRESS "{}" CACHE STRING "Address to use for ADVR cloud Gitlab git repositories" FORCE)\n'.format(args.gitlab_advrcloud_base_address))
     f.write('set(YCM_GIT_GITLAB_ADVR_BASE_ADDRESS "{}" CACHE STRING "Address to use for ADVR Gitlab git repositories" FORCE)\n\n'.format(args.gitlab_advr_base_address))
 
